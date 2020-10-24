97d6e1f1e41e3c90b2464d66a4e5bc38a0931010
Ana Calabano <acalabano@circleblack.com>
2020-10-24 06:30:00 -0400

    stream worked better source bug

--- eventBot.py
+++ eventBot.py
@@ -86,6 +86,11 @@ def play_next(ctx):
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
