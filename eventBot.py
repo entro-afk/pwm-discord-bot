3285f9d2c586c8d7b23a391a59ad3897fa48e850
Ana Calabano <acalabano@circleblack.com>
2020-10-24 05:36:32 -0400

    ytld format

--- eventBot.py
+++ eventBot.py
@@ -86,14 +86,6 @@ def play_next(ctx):
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
