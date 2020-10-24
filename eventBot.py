9ab618911307ac05822c9c2b147a365da0e1eae7
Ana Calabano <acalabano@circleblack.com>
2020-10-24 03:11:03 -0400

    predownload yes checking link

--- eventBot.py
+++ eventBot.py
@@ -206,7 +206,7 @@ def current_song_finished(e):
 
 async def handle_player_emoji(message, emoji, author):
     if message.embeds[0].video:
-        print('url------------')
+        print('url------------', message.embeds[0])
         print(message.embeds[0].video.url)
         if emoji.name == '▶':
             await play_on_channel(message.embeds[0].video.url, author.voice.channel, message.guild, message)
